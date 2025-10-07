62546e376cb4bbeefd26b3ef4fa2902a06684132
Shivansh1980 <shivanshshrivastava1980@gmail.com>
2025-10-07 18:54:44 +0530

    added urls.py

--- paperiq_backend/paperiq_ai/urls.py
+++ paperiq_backend/paperiq_ai/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import get_summary
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('paperiq_ai.urls'))
+    path('get-summary/', get_summary)
 ]
